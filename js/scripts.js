0335b1dac2273b3024ba40ba22b2d13c2281d956
michellevillarante <michellevillarante16@gmail.com>
2026-07-02 13:56:07 +0800

    add 3rd number

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,12 @@
 function calcSum(){
     const num1=document.getElementById('num1').value;
     const num2=document.getElementById('num2').value;
+    const num3=document.getElementById('num3').value;
 
     const n1 = parseInt(num1);
     const n2 = parseInt(num2);
-    const sum = n1 + n2;
+    const n3 = parseInt(num3);
+    const sum = n1 + n2 + n3;
 
     //Display the result
     document.getElementById('answer').textContent = sum;
@@ -13,4 +15,6 @@ function calcSum(){
 function Clear(){
     document.getElementById('num1').value="";
     document.getElementById('num2').value="";
+    document.getElementById('num3').value="";
+    document.getElementById('answer').textContent = " ";
 }
